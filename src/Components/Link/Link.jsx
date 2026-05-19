8fd5b1f013ec85e5cc9cc727515d0ec4094cee79
Samia123-source <samiamahim04@gmail.com>
2026-05-19 15:46:52 +0600

    daisy UI

---
+++ src/Components/Link/Link.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types'; // ES6
+const Link = ({route}) => {
+    return (
+      <li className="mr-10">
+                    <a href={route.path}>{route.name}</a></li>
+      );
+};
+ Link.PropTypes = {
+    route: PropTypes.object
+ }
+export default Link;
