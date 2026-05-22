cf7be724c90367cdeb454b815085c95ab120fd3a
Samia123-source <samiamahim04@gmail.com>
2026-05-22 23:41:47 +0600

    Responsive pricing card with Tailwind and react icons

--- src/Components/PriceOptions/PriceOptions.jsx
+++ src/Components/PriceOptions/PriceOptions.jsx
@@ -76,7 +76,7 @@ const priceOptions =
     return (
         <div className="m-12">
             <h2 className="text-5xl">Best Prices in the town</h2>
-           <div className="grid grid-cols-3 gap-6">
+           <div className="grid md:grid-cols-3 gap-6">
              {
                 priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
             }
